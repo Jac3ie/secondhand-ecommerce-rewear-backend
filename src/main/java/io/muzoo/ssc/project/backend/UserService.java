553711753eabce8c5d2c8ecc807bcfc6c70d19ef
sathitsc <sathitseth.c@gmail.com>
2025-03-15 20:00:34 +0700

    Save registered user and Validation

--- src/main/java/io/muzoo/ssc/project/backend/UserService.java
+++ src/main/java/io/muzoo/ssc/project/backend/UserService.java
@@ -1,4 +1,26 @@
 package io.muzoo.ssc.project.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean registerUser(User user) {
+        if (userRepository.findFirstByUsername(user.getUsername()) != null) {
+            return false; // Username is taken
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true; // Successfully registered
+    }
 }
